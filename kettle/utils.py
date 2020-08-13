e08c74bc26efe2ba8e54d8bea7c471e9cba284ac
Andreas Backström <mozzo242@gmail.com>
2020-08-13 14:55:21 +0200

    Revert "Change icons when theme changes"
    This reverts commit a0c861255241ea6cd7618fe42b20cfe110a419e4.

--- kettle/utils.py
+++ kettle/utils.py
@@ -1,14 +1,4 @@
 import os
-from PyQt5.QtGui import QIcon
-
-basedir = os.path.abspath(os.path.dirname(__file__))
-
-
-def select_asset_based_on_theme(white_theme_icon, dark_theme_icon, configparser):
-    if str2bool(configparser.get_setting('General', 'use_dark_theme')):
-        return QIcon(os.path.join(basedir, f'../assets/{dark_theme_icon}'))
-    else:
-        return QIcon(os.path.join(basedir, f'../assets/{white_theme_icon}'))
 
 
 def str2bool(str):
