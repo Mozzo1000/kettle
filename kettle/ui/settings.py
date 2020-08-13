39fbbe595881c31794ce630f2802dd75cd49b254
Andreas Backström <mozzo242@gmail.com>
2020-08-13 16:17:24 +0200

    Add theme selector

--- kettle/ui/settings.py
+++ kettle/ui/settings.py
@@ -1,14 +1,15 @@
 import os
 from utils import str2bool
 from config import Config
-from PyQt5.QtWidgets import QMainWindow, QLabel, QPushButton, QCheckBox, QFontDialog, QMessageBox
+from PyQt5.QtWidgets import QMainWindow, QLabel, QPushButton, QCheckBox, QFontDialog, QMessageBox, QComboBox
 
 config = Config(os.path.expanduser('~/.kettle/'), 'config.ini')
 
 
 class Settings(QMainWindow):
-    def __init__(self, parent):
+    def __init__(self, parent, themes):
         super().__init__(parent)
+        self.themes = themes
         self.init_ui()
 
     def save_settings(self):
@@ -29,12 +30,25 @@ class Settings(QMainWindow):
             config.update_config('General', 'font', font.toString().split(',')[0])
             QMessageBox.question(self, 'Info', 'Please restart the application for changes to take affect.', QMessageBox.Close)
 
+    def change_theme(self):
+        config.update_config('General', 'theme', self.theme_selector.currentText())
+        print('Theme selected: ' + self.theme_selector.currentText())
+
     def init_ui(self):
         self.resize(200, 200)
         self.show_statusbar = QCheckBox("Show statusbar", self)
 
         if str2bool(config.get_setting('view', 'view_statusbar')):
             self.show_statusbar.setChecked(True)
+        theme_label = QLabel("Theme:", self)
+        theme_label.move(0, 50)
+        self.theme_selector = QComboBox(self)
+        self.theme_selector.move(50, 50)
+        self.theme_selector.addItems(self.themes.get_all())
+        self.theme_selector.setCurrentText(config.get_setting('General', 'theme'))
+        self.theme_selector.currentTextChanged.connect(self.change_theme)
+
+
         save_btn = QPushButton("Save", self)
         save_btn.move(0, 100)
         save_btn.setToolTip("Save settings")
