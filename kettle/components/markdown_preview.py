dbfc740bbb93ce47e96367e2fd18a8a5d346fa7c
Andreas Backström <mozzo242@gmail.com>
2020-08-14 21:33:30 +0200

    Add scrollbar sync to markdown preview
    When you now scroll in the textarea, the scrollbar in the preview window will follow.

--- kettle/components/markdown_preview.py
+++ kettle/components/markdown_preview.py
@@ -11,9 +11,15 @@ class MarkdownPreview(QDockWidget):
 
         text.textChanged.connect(self.on_update)
 
+        self.text.verticalScrollBar().valueChanged.connect(self.sync_scroll)
+
         self.web = QWebEngineView()
         self.web.setHtml(markdown2.markdown(self.text.toPlainText(), extras=['tables']))
+        self.web.loadFinished.connect(self.sync_scroll)
         self.setWidget(self.web)
+
+    def sync_scroll(self):
+        self.web.page().runJavaScript(f'scrollTo({self.text.horizontalScrollBar().value()}, document.body.scrollHeight * {self.text.verticalScrollBar().value()} / {self.text.verticalScrollBar().maximum()});')
 
     def on_update(self):
         self.web.page().setHtml(markdown2.markdown(self.text.toPlainText(), extras=['tables']))
