01283c874201343c81c35dc59a997d5e244f5c96
Andreas Backström <mozzo242@gmail.com>
2020-08-15 13:35:33 +0200

    Add more extras to markdown preview

--- kettle/components/markdown_preview.py
+++ kettle/components/markdown_preview.py
@@ -10,13 +10,14 @@ class MarkdownPreview(QDockWidget):
         self.file = file
         self.text = text
         self.setWindowTitle('Markdown Preview')
+        self.markdown_extras = ['tables', 'fenced-code-blocks', 'task-list']
 
         text.textChanged.connect(self.on_update)
 
         self.text.verticalScrollBar().valueChanged.connect(self.sync_scroll)
 
         self.web = QWebEngineView()
-        self.web.setHtml(markdown2.markdown(self.text.toPlainText(), extras=['tables']), baseUrl=QUrl.fromLocalFile(file))
+        self.web.setHtml(markdown2.markdown(self.text.toPlainText(), extras=self.markdown_extras), baseUrl=QUrl.fromLocalFile(file))
         self.web.loadFinished.connect(self.sync_scroll)
         self.setWidget(self.web)
 
@@ -24,4 +25,4 @@ class MarkdownPreview(QDockWidget):
         self.web.page().runJavaScript(f'scrollTo({self.text.horizontalScrollBar().value()}, document.body.scrollHeight * {self.text.verticalScrollBar().value()} / {self.text.verticalScrollBar().maximum()});')
 
     def on_update(self):
-        self.web.page().setHtml(markdown2.markdown(self.text.toPlainText(), extras=['tables']), baseUrl=QUrl.fromLocalFile(self.file))
+        self.web.page().setHtml(markdown2.markdown(self.text.toPlainText(), extras=self.markdown_extras), baseUrl=QUrl.fromLocalFile(self.file))
