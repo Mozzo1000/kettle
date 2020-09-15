b20b69514e1e1bdea59e7aca6b9d27c651d0fa60
Andreas Backström <mozzo242@gmail.com>
2020-09-15 19:26:44 +0200

    Add function to convert byte sizes
    Converts given byte to more human friendly output (MB, GB etc)

--- kettle/utils.py
+++ kettle/utils.py
@@ -1,4 +1,5 @@
 import os
+import math
 from PyQt5.QtGui import QIcon
 
 basedir = os.path.abspath(os.path.dirname(__file__))
@@ -6,3 +7,13 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 def str2bool(str):
     return str.lower() in ('True', 'true', 'yes', '1')
+
+
+def convert_size(size_bytes):
+    if size_bytes == 0:
+        return "0B"
+    size_name = ("B", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB")
+    i = int(math.floor(math.log(size_bytes, 1024)))
+    p = math.pow(1024, i)
+    s = round(size_bytes / p, 2)
+    return "%s %s" % (s, size_name[i])
